19eb9a6b005fd1f9a2acbdbd559584d5a46190ba
Nadineioes <god0003@jmss.vic.edu.au>
2016-10-13 22:18:18 +1100

    Update summarynadine.py

--- summarynadine.py
+++ summarynadine.py
@@ -1,63 +1,101 @@
 import csv
 
 file = open('Actigraphy data_Adult.csv')
-bedtimes = []
-min_bedtime = ()
-max_bedtime = ()
-avg_bedtime = ()
 
-get_up_times = []
-min_get_up_time = ()
-max_get_up_time = ()
-avg_get_up_time = ()
+sleep_start_times = []
+sleep_end_times = []
+sleep_durations = []
 
-time_in_beds = []
-min_time_in_bed = ()
-max_time_in_bed = ()
-avg_time_in_bed = ()
+def time_24hr(time):
+    if 'PM' in time:
+        time = time.strip(' PM')
+        time = time.split(':')
+        if int(time[0]) < 12:
+            time[0] = int(time[0]) + 12
+            time[0] = str(time[0])
+        time = ":".join(time)
+        return(time)
+    elif 'AM' in time:
+        time = time.strip(' AM')
+        time = time.split(':')
+        if int(time[0]) == 12:
+            time[0] = 0
+            time[0] = str(time[0])
+        time = ":".join(time)
+        return(time)
 
-total_sleep_times = []
-min_total_sleep_time = ()
-max_total_sleep_time = ()
-avg_total_sleep_time = ()
+def string_to_list(time):
+    time = time.split(':')
+    time[0] = int(time[0])
+    time[1] = int(time[1])
+    time[2] = int(time[2])
+    return(time)
 
-onset_latencys = []
-min_onset_latency = ()
-max_onset_latency = ()
-avg_onset_latency = ()
+for line in file:
+    line = line.strip('"')
+    line = line.strip('",\n')
+    line = line.split('","')
 
-sleep_efficiencys = []
-min_sleep_efficiency = ()
-max_sleep_efficiency = ()
-avg_sleep_efficiency = ()
+#Extracting row by row of recorded sleep times
+    if 'SLEEP' in line:
+#Append start times
+        time1 = time_24hr(line[4])
+        sleep_start_times.append(time1)
 
-wasos = []
-min_waso = ()
-max_waso = ()
-avg_waso = ()
+#Append end time
+        time2 = time_24hr(line[7])
+        sleep_end_times.append(time2)
 
-awaks = []
-min_awak = ()
-max_awak = ()
-avg_awak = ()
+#Calculate duration and append to sleep_durations
+        time1 = [int(part) for part in time1.split(":")]
+        time2 = [int(part) for part in time2.split(":")]
+        time3 = [0,0,0]
+        if time1[0] > time2[0]:
+            hour_difference = 24 - time1[0]
+            time3[0] = time3[0] + hour_difference
+        time3[0] = time3[0] + time2[0]
+        minute_difference = time1[1] - time2[1]
+        if minute_difference < 0:
+            time3[0] = time3[0] - 1
+            time3[1] = 60 + minute_difference
+        else:
+            time3[1] = minute_difference
+        sleep_durations.append(time3)
 
-for i in file:
-    i = i.split(',')
 
-    #print(i)
-    #print(file)
-    if '------------------------ Statistics ------------------------' in i[0]:
-        break
-for i in file:
-    collum = 0
-    i = i.split(',')
-    for collum in i:
-        print(collum)
-    #print(i)
-    if 'SLEEP' in i[0]:
-        #print(i[4])
-        bedtimes.append(i[4])
-    if '-------------------- Epoch-by-Epoch Data -------------------' in i[0]:
-        break
+#Minimums for time values - incomplete
+    min_bedtime = min(sleep_start_times)
+    min_get_up_time = min(sleep_end_times)
+    min_hours_of_sleep = min(sleep_durations)
 
-print(bedtimes)
+#Maximums for time values - incomplete
+    max_bedtime = max(sleep_start_times)
+    max_get_up_time = max(sleep_end_times)
+    max_hours_of_sleep = max(sleep_durations)
+
+#Averages for time values - incomplete
+    avg_bedtime = avg(sleep_start_times)
+    avg_get_up_time = avg(sleep_end_times)
+    avg_hours_of_sleep = avg(sleep_durations)
+
+#Minimums for all except time
+    if 'Sleep Summary' in line and 'Minimum(n)' in line:
+        min_onset_latency = line[20]
+        min_efficiency = line[21]
+        min_waso = line[23]
+        min_awakenings = line[25]
+
+#Maximums for all except time
+    if 'Sleep Summary' in line and 'Maximum(n)' in line:
+        max_onset_latency = line[20]
+        max_efficiency = line[21]
+        max_waso = line[23]
+        max_awakenings = line[25]
+
+#Averages for all except time
+    if 'Sleep Summary' in line and 'Average(n)' in line:
+        #print(line)
+        avg_onset_latency = line[20]
+        avg_efficiency = line[21]
+        avg_waso = line[23]
+        avg_awakenings = line[25]
